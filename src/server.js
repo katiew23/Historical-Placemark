0fd4849db0f9a4926ce59c4603e6f5c29aa89c03
Kate Williamscd <katiew23@gmail.com>
2026-03-10 19:28:37 +0000

    Resolve deployment issues: fix Joi dependency, configure environment variables, and enable Render deployment

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ const swaggerOptions = {    //this needs to be added first to ensure that the se
 async function init() {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
-    host: "0.0.0.0",
+    //host: "0.0.0.0",
   });
 
   await server.register([
