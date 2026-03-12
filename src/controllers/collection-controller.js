2c67a15e606e9fe3a56fa00a05aad9fd7d0fcabe
Kate Williamscd <katiew23@gmail.com>
2026-03-12 14:18:06 +0000

    Add Firebase Firestore models and tests, and admin API tests

--- src/controllers/collection-controller.js
+++ src/controllers/collection-controller.js
@@ -2,6 +2,7 @@ import { db } from "../models/db.js";
 import { PlacemarkSpec } from "../models/joi-schemas.js";
 import { imageStore } from "../models/image-store.js";
 
+
 export const collectionController = {
 
   index: {
@@ -34,35 +35,35 @@ export const collectionController = {
   },
 
   addPlacemark: {
-    validate: {
-      payload: PlacemarkSpec,
-      options: { abortEarly: false },
-      failAction: function (request, h, error) {
-        return h.view("collection-view", {
-          title: "Add placemark error",
-          errors: error.details
-        }).takeover().code(400);
-      }
-    },
-
-    handler: async function (request, h) {
-      const collection = await db.collectionStore.getCollectionById(request.params.id);
-
-      const newPlacemark = {
-        name: request.payload.name,
-        description: request.payload.description,
-        latitude: Number(request.payload.latitude),
-        longitude: Number(request.payload.longitude),
-        category: request.payload.category,
-        yearEstablished: Number(request.payload.yearEstablished),
-        county: request.payload.county
-      };
-
-      await db.placemarkStore.addPlacemark(collection._id, newPlacemark);
-
-      return h.redirect(`/collection/${collection._id}`);
+  validate: {
+    payload: PlacemarkSpec,
+    options: { abortEarly: false },
+    failAction: function (request, h, error) {
+      return h.view("collection-view", {
+        title: "Add placemark error",
+        errors: error.details
+      }).takeover().code(400);
     }
   },
+
+  handler: async function (request, h) {
+    const collection = await db.collectionStore.getCollectionById(request.params.id);
+
+    const newPlacemark = {
+      name: request.payload.name,
+      description: request.payload.description,
+      latitude: Number(request.payload.latitude),
+      longitude: Number(request.payload.longitude),
+      category: request.payload.category,
+      yearEstablished: Number(request.payload.yearEstablished),
+      county: request.payload.county
+    };
+
+    await db.placemarkStore.addPlacemark(collection._id, newPlacemark);
+
+    return h.redirect(`/collection/${collection._id}`);
+  }
+},
 
   deletePlacemark: {
     handler: async function (request, h) {
