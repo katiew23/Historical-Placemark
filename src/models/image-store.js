00a51c1a3e6a1e5bfa05928887b2447e4fd8351e
Kate Williamscd <katiew23@gmail.com>
2026-03-13 15:34:24 +0000

    fix cloudinary v2 configuration

--- src/models/image-store.js
+++ src/models/image-store.js
@@ -1,32 +1,29 @@
-import * as cloudinary from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const credentials = {
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
-};
-
-cloudinary.config(credentials);
+});
 
 export const imageStore = {
 
   getAllImages: async function () {
-    const result = await cloudinary.v2.api.resources();
+    const result = await cloudinary.api.resources();
     return result.resources;
   },
 
   uploadImage: async function (imagefile) {
     console.log("UPLOAD FUNCTION RUNNING");
-    const response = await cloudinary.v2.uploader.upload(imagefile.path);
+    const response = await cloudinary.uploader.upload(imagefile.path);
     return response.url;
   },
 
   deleteImage: async function (img) {
-    await cloudinary.v2.uploader.destroy(img, {});
+    await cloudinary.uploader.destroy(img);
   }
 };
-
 // keeps storage logic away from controller
